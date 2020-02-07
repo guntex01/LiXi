a6fc0e438dca2317c92c86207a9067c37f3efbfa
guntex01 <guntex01@guntex01s-MBP.net.fpt>
2020-02-08 01:56:40 +0700

    abc

---
+++ LiXi/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  LiXi
+//
+//  Created by guntex01 on 2/8/20.
+//  Copyright © 2020 guntex01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var soTienLabel: UILabel!
+    @IBOutlet weak var baoLiXiImageView: UIImageView!
+    @IBOutlet weak var openButton: UIButton!
+    var isOpen: Bool = true
+    var n: Int = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        baoLiXiImageView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height/4 * 3)
+        baoLiXiImageView.image = UIImage(named: "lixi.png" )
+        openButton.setTitle("Mở lì xì", for: .normal)
+        isOpen = true
+        
+    }
+
+    @IBAction func onPress(_ sender: Any) {
+        if isOpen{
+                    baoLiXiImageView.image = UIImage(named: "tienlixi.png")
+                    openButton.setTitle("Chúc mừng bạn", for: .normal)
+                    isOpen = false
+                }else{
+                    baoLiXiImageView.image = UIImage(named: "tienlixi.png")
+                    openButton.setTitle("Mở lì xì", for: .normal)
+                    isOpen = true
+    }
+        soTienLabel.text = String(ranDomTien())
+}
+    func ranDomTien() -> Int{
+        let m = Int.random(in: 1000...1000000)
+        
+        if n == m {
+            ranDomTien()
+        }
+        n = m
+        return m
+        
+    }
+    }
+
+
